0520b7f285d7a54c3710108a40a7e8d18516dae8
CapraLinnaeus <caprahircuslinnaeus@gmail.com>
2020-11-09 14:57:54 +0200

    add styles

--- view/read.php
+++ view/read.php
@@ -2,6 +2,7 @@
 <html lang="en">x
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/main.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -12,9 +13,10 @@
 <button type="submit" class="btn btn-primary">Editer</button>
 <button type="submit" class="btn btn-danger">X</button>
 
+<div class="container border rounded shadow">
+<center><h1  style="font-family:  Georgia, serif; margin-top: 15px;">Chapître 1: Le voyage de Moricio</h1></center>
 
-<center><h1>Chapître 1: Le voyage de Moricio</h1></center>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Quis risus sed vulputate odio ut enim blandit volutpat. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Non odio euismod lacinia at quis risus sed. Venenatis a condimentum vitae sapien pellentesque. Donec et odio pellentesque diam volutpat commodo sed. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Quam vulputate dignissim suspendisse in est ante. Mollis nunc sed id semper risus in. Eget lorem dolor sed viverra ipsum. Nec ullamcorper sit amet risus nullam eget felis eget nunc. Et magnis dis parturient montes nascetur ridiculus mus mauris. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus.
+<p class="text-justify" style="margin-left: 30px; margin-right: 30px;"> <br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Quis risus sed vulputate odio ut enim blandit volutpat. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Non odio euismod lacinia at quis risus sed. Venenatis a condimentum vitae sapien pellentesque. Donec et odio pellentesque diam volutpat commodo sed. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Quam vulputate dignissim suspendisse in est ante. Mollis nunc sed id semper risus in. Eget lorem dolor sed viverra ipsum. Nec ullamcorper sit amet risus nullam eget felis eget nunc. Et magnis dis parturient montes nascetur ridiculus mus mauris. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus.
 
     Rutrum quisque non tellus orci. Risus viverra adipiscing at in tellus. Tincidunt ornare massa eget egestas purus viverra accumsan. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Convallis a cras semper auctor. Sed euismod nisi porta lorem mollis. Sit amet risus nullam eget felis eget nunc lobortis. Duis ut diam quam nulla porttitor massa id neque. Tellus in metus vulputate eu scelerisque felis imperdiet proin. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. At imperdiet dui accumsan sit amet. Duis tristique sollicitudin nibh sit. Ut morbi tincidunt augue interdum. Tellus pellentesque eu tincidunt tortor aliquam. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Blandit libero volutpat sed cras ornare arcu dui vivamus arcu.
 
@@ -24,6 +26,10 @@
 
     Donec massa sapien faucibus et molestie. Habitant morbi tristique senectus et netus et malesuada. Orci a scelerisque purus semper eget. Fringilla urna porttitor rhoncus dolor purus non enim praesent elementum. In eu mi bibendum neque egestas congue quisque egestas diam. Euismod elementum nisi quis eleifend quam adipiscing vitae proin. Semper viverra nam libero justo laoreet sit amet cursus. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Pulvinar elementum integer enim neque volutpat ac. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Purus non enim praesent elementum facilisis leo. Nulla aliquet porttitor lacus luctus accumsan tortor. Facilisi nullam vehicula ipsum a arcu cursus vitae. At erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Tempor id eu nisl nunc mi ipsum faucibus.</p>
 
+    <button type="button" class="btn btn-info" style="margin-left: 30px;">Chapître précédent</button>  <button type="button" class="btn btn-info" style="margin-left: 60%; margin-right: ">Chapître suivant</button>
+
+
+</div> <br/>
 <div class="container border rounded shadow" style="height:120px; margin-bottom: 15px; background-color: #f6f6f6;">
     <strong>Pseudo1</strong> Incroyable histoire j'adore!  <button type="button" class="btn btn-danger">Report</button> <button type="button" class="btn btn-danger">X</button>
 </div>
